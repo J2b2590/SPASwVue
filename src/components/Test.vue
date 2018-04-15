dbf835a90f71f839787bddc9efc63ed423cd33c7
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-04-14 22:08:43 -0500

    learning directives and template logic

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,45 @@
 <template>
-	<div>
-		<h1>Testing one two</h1>
+	<div class='test'>
+		<input type='text' v-model="title"><br />
+		<h1>{{title}}</h1>
+		<p v-if='showName'>{{user.firstName}}</p>
+		<p v-else>nobody</p>
+		<ul>
+			<li v-for='item in items'>{{item.title}}</li>
+		</ul>
+		<button v-on:click="greeting('Hello World')"> say greeting</button>
+		<br />
+		<input type='text' v-on:keyup="pressKey">
 	</div>
 </template>
 
 
 <script>
 	export default {
-		name: 'test'
+		name: 'test',
+		data() {
+			return {
+				title: "Hello World",
+				user: {
+					firstName: 'Jon',
+					lastName: 'Doe'
+				},
+				showName: true,
+				items: [
+				{title:'Item One'},
+				{title:'Item Two'},
+				{title:'Item Three'},
+				]
+			}
+		},
+		methods: {
+			greeting: function(greet){
+				alert(greet)
+			},
+			pressKey: function(e){
+				console.log(e.target.value);
+			}
+		}
 	}
 </script>
 

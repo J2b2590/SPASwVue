5c3ec2c52c80c86c78c23b754566d5217915b356
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-04-15 22:13:19 -0500

    resource and router modules added

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,22 @@
 <template>
 	<div class='users'>
-	<h1>Users</h1>
+
+		<h1>Users</h1>
+		<form v-on:submit="addUser">
+			<input type="text" v-model="newUser.name" placeholder="Enter Name">
+			<br />
+			<input type="text" v-model="newUser.email" placeholder="Enter Email">
+			<br />
+			<input type="submit" value="submit">
+		</form>
+		<ul>
+			<li v-for="user in users">
+				<input type="checkbox" class="toggle" v-model="user.contacted">
+				<span :class="{contacted: user.contacted}">
+					{{user.name}} : {{user.email}} <button v-on:click="deleteUser(user)">x</button>
+				</span>
+			</li>
+		</ul>
 		
 	</div>
 </template>
@@ -13,6 +29,7 @@
 		data() {
 
 			return {
+				newUser:{},
 				users: [
 				{
 					name: "Jon Doe",
@@ -21,7 +38,7 @@
 				},
 				{
 					name: "Jane Doe",
-					email: "JaneDoe@gmail.com",
+					email: "JaD@gmail.com",
 					contacted: false
 				},
 				{
@@ -34,8 +51,24 @@
 		}
 	},
 		methods: {
-			
+			addUser: function(e){
+				this.users.push({
+					name: this.newUser.name,
+					email: this.newUser.email,
+					contacted: false
+				});
+				e.preventDefault();
+			},
+			deleteUser: function(user){
+				this.users.splice(this.users.indexOf(user), 1);
+			}
 		},
+		created: function(){
+			this.$http.get('https://jsonplaceholder.typicode.com/users')
+				.then(function(response){
+					this.users = response.data
+				});
+		}
 		
 	}
 </script>
@@ -43,4 +76,22 @@
 
 <style scoped>
 
+.contacted{
+	text-decoration: line-through;
+}
+
 </style> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
